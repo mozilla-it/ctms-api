0f31388406f206141d0bf333d122b812237b1ff7
Brian Stack <bstack@mozilla.com>
2021-02-03 13:15:43 -0800

    Apply template updates from containerized-microservice-template

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,5 +5,8 @@ set -e
 CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 BASE_DIR="$(dirname "$CURRENT_DIR")"
 
+bandit -lll --recursive "${BASE_DIR}" --exclude "${BASE_DIR}/poetry.lock,${BASE_DIR}/.venv,${BASE_DIR}/.mypy,${BASE_DIR}/build"
+detect-secrets-hook {./*,**/**} --baseline .secrets.baseline
+mypy --no-strict-optional --ignore-missing-imports "${BASE_DIR}"
 black --config "${BASE_DIR}/pyproject.toml" "${BASE_DIR}"
 isort --recursive --settings-path "${BASE_DIR}/pyproject.toml" "${BASE_DIR}"
---
+++ scripts/update_baseline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+BASE_DIR="$(dirname "$CURRENT_DIR")"
+
+detect-secrets scan {./*,**/**} --exclude-files poetry.lock,htmlcov/status.json > .secrets.baseline
