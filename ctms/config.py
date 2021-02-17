cbd5e90b6c5eb2d050b6538655449f947d923b46
Brian Stack <bstack@mozilla.com>
2021-02-17 10:10:01 -0800

    Use a more specific schema for db_url

--- ctms/config.py
+++ ctms/config.py
@@ -1,8 +1,8 @@
-from pydantic import BaseSettings
+from pydantic import BaseSettings, PostgresDsn
 
 
 class Settings(BaseSettings):
-    db_url: str
+    db_url: PostgresDsn
 
     class Config:
         env_prefix = "ctms_"
