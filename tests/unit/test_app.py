fe643b43afc6800a492bfe836ade4958ff427eb8
John Whitlock <jwhitlock@mozilla.com>
2021-02-25 10:20:10 -0600

    Migrate basic app tests to pytest

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -9,3 +9,10 @@ def test_read_root(client):
     prev_resp = resp.history[0]
     assert prev_resp.status_code == 307  # Temporary Redirect
     assert prev_resp.headers["location"] == "./docs"
+
+
+def test_read_health(client):
+    """The platform calls /health to check app readiness."""
+    resp = client.get("/health")
+    assert resp.status_code == 200
+    assert resp.json() == [{"health": "OK"}, 200]
