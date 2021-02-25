74acba8b45c3b08a7636ede05879e27b5c92813d
John Whitlock <jwhitlock@mozilla.com>
2021-02-25 13:00:12 -0600

    Convert API tests to pytest

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -14,7 +14,200 @@ from ctms.models import Email
 from ctms.sample_data import SAMPLE_CONTACTS
 
 
-def test_api_example(client, example_contact):
+def test_get_ctms_for_minimal_contact(client, minimal_contact):
+    """GET /ctms/{email_id} returns a contact with most fields unset."""
+    email_id = minimal_contact.email.email_id
+    resp = client.get(f"/ctms/{email_id}")
+    assert resp.status_code == 200
+    assert resp.json() == {
+        "amo": {
+            "add_on_ids": None,
+            "create_timestamp": None,
+            "display_name": None,
+            "email_opt_in": False,
+            "language": None,
+            "last_login": None,
+            "location": None,
+            "profile_url": None,
+            "update_timestamp": None,
+            "user": False,
+            "user_id": None,
+            "username": None,
+        },
+        "email": {
+            "basket_token": "142e20b6-1ef5-43d8-b5f4-597430e956d7",
+            "create_timestamp": "2014-01-22T15:24:00+00:00",
+            "email_format": "H",
+            "email_id": "93db83d4-4119-4e0c-af87-a713786fa81d",
+            "email_lang": "en",
+            "first_name": None,
+            "has_opted_out_of_email": False,
+            "last_name": None,
+            "mailing_country": "us",
+            "mofo_id": None,
+            "mofo_relevant": False,
+            "primary_email": "ctms-user@example.com",
+            "sfdc_id": "001A000001aABcDEFG",
+            "unsubscribe_reason": None,
+            "update_timestamp": "2020-01-22T15:24:00+00:00",
+        },
+        "fxa": {
+            "created_date": None,
+            "account_deleted": False,
+            "first_service": None,
+            "fxa_id": None,
+            "lang": None,
+            "primary_email": None,
+        },
+        "newsletters": [
+            {
+                "format": "H",
+                "lang": "en",
+                "name": "app-dev",
+                "source": None,
+                "subscribed": True,
+                "unsub_reason": None,
+            },
+            {
+                "format": "H",
+                "lang": "en",
+                "name": "maker-party",
+                "source": None,
+                "subscribed": True,
+                "unsub_reason": None,
+            },
+            {
+                "format": "H",
+                "lang": "en",
+                "name": "mozilla-foundation",
+                "source": None,
+                "subscribed": True,
+                "unsub_reason": None,
+            },
+            {
+                "format": "H",
+                "lang": "en",
+                "name": "mozilla-learning-network",
+                "source": None,
+                "subscribed": True,
+                "unsub_reason": None,
+            },
+        ],
+        "status": "ok",
+        "vpn_waitlist": {"geo": None, "platform": None},
+    }
+
+
+def test_get_ctms_for_maximal_contact(client, maximal_contact):
+    """GET /ctms/{email_id} returns a contact with almost all fields set."""
+    email_id = maximal_contact.email.email_id
+    resp = client.get(f"/ctms/{email_id}")
+    assert resp.status_code == 200
+    assert resp.json() == {
+        "amo": {
+            "add_on_ids": "fanfox,foxfan",
+            "create_timestamp": "2017-05-12T15:16:00+00:00",
+            "display_name": "#1 Mozilla Fan",
+            "email_opt_in": True,
+            "language": "fr,en",
+            "last_login": "2020-01-27",
+            "location": "The Inter",
+            "profile_url": "firefox/user/14508209",
+            "update_timestamp": "2020-01-27T14:25:43+00:00",
+            "user": True,
+            "user_id": "123",
+            "username": "Mozilla1Fan",
+        },
+        "email": {
+            "basket_token": "d9ba6182-f5dd-4728-a477-2cc11bf62b69",
+            "create_timestamp": "2010-01-01T08:04:00+00:00",
+            "email_format": "H",
+            "email_id": "67e52c77-950f-4f28-accb-bb3ea1a2c51a",
+            "email_lang": "fr",
+            "first_name": "Fan",
+            "has_opted_out_of_email": False,
+            "last_name": "of Mozilla",
+            "mailing_country": "ca",
+            "mofo_id": "195207d2-63f2-4c9f-b149-80e9c408477a",
+            "mofo_relevant": True,
+            "primary_email": "mozilla-fan@example.com",
+            "sfdc_id": "001A000001aMozFan",
+            "unsubscribe_reason": "done with this mailing list",
+            "update_timestamp": "2020-01-28T14:50:00+00:00",
+        },
+        "fxa": {
+            "created_date": "2019-05-22T08:29:31.906094+00:00",
+            "account_deleted": False,
+            "first_service": "monitor",
+            "fxa_id": "611b6788-2bba-42a6-98c9-9ce6eb9cbd34",
+            "lang": "fr,fr-CA",
+            "primary_email": "fxa-firefox-fan@example.com",
+        },
+        "newsletters": [
+            {
+                "format": "H",
+                "lang": "en",
+                "name": "ambassadors",
+                "source": "https://www.mozilla.org/en-US/contribute/studentambassadors/",
+                "subscribed": False,
+                "unsub_reason": "Graduated, don't have time for FSA",
+            },
+            {
+                "format": "T",
+                "lang": "fr",
+                "name": "common-voice",
+                "source": "https://commonvoice.mozilla.org/fr",
+                "subscribed": True,
+                "unsub_reason": None,
+            },
+            {
+                "format": "H",
+                "lang": "fr",
+                "name": "firefox-accounts-journey",
+                "source": "https://www.mozilla.org/fr/firefox/accounts/",
+                "subscribed": False,
+                "unsub_reason": "done with this mailing list",
+            },
+            {
+                "format": "H",
+                "lang": "en",
+                "name": "firefox-os",
+                "source": None,
+                "subscribed": True,
+                "unsub_reason": None,
+            },
+            {
+                "format": "H",
+                "lang": "fr",
+                "name": "hubs",
+                "source": None,
+                "subscribed": True,
+                "unsub_reason": None,
+            },
+            {
+                "format": "H",
+                "lang": "en",
+                "name": "mozilla-festival",
+                "source": None,
+                "subscribed": True,
+                "unsub_reason": None,
+            },
+            {
+                "format": "H",
+                "lang": "fr",
+                "name": "mozilla-foundation",
+                "source": None,
+                "subscribed": True,
+                "unsub_reason": None,
+            },
+        ],
+        "status": "ok",
+        "vpn_waitlist": {"geo": "ca", "platform": "windows,android"},
+    }
+
+
+def test_get_ctms_for_api_example(client, example_contact):
+    """The API examples represent a valid contact with many fields set."""
     """Test that the API examples are valid."""
     email_id = example_contact.email.email_id
     resp = client.get(f"/ctms/{email_id}")
@@ -82,6 +275,14 @@ def test_api_example(client, example_contact):
     }
 
 
+def test_get_ctms_not_found(client, dbsession):
+    """GET /ctms/{unknown email_id} returns a 404."""
+    email_id = "cad092ec-a71a-4df5-aa92-517959caeecb"
+    resp = client.get(f"/ctms/{email_id}")
+    assert resp.status_code == 404
+    assert resp.json() == {"detail": "Unknown email_id"}
+
+
 @pytest.mark.parametrize(
     "alt_id_name,alt_id_value",
     [
@@ -95,7 +296,7 @@ def test_api_example(client, example_contact):
         ("mofo_id", "195207d2-63f2-4c9f-b149-80e9c408477a"),
     ],
 )
-def test_ctms_by_alt_id(sample_contacts, client, alt_id_name, alt_id_value):
+def test_get_ctms_by_alt_id(sample_contacts, client, alt_id_name, alt_id_value):
     """The desired contact can be fetched by alternate ID."""
     maximal_id, contact = sample_contacts["maximal"]
     resp = client.get("/ctms", params={alt_id_name: alt_id_value})
@@ -103,6 +304,15 @@ def test_ctms_by_alt_id(sample_contacts, client, alt_id_name, alt_id_value):
     data = resp.json()
     assert len(data) == 1
     assert data[0]["email"]["email_id"] == str(maximal_id)
+
+
+def test_get_ctms_by_no_ids_is_error(client, dbsession):
+    """Calling GET /ctms with no ID query is an error."""
+    resp = client.get("/ctms")
+    assert resp.status_code == 400
+    assert resp.json() == {
+        "detail": "No identifiers provided, at least one is needed: email_id, primary_email, basket_token, sfdc_id, mofo_id, amo_user_id, fxa_id, fxa_primary_email"
+    }
 
 
 @pytest.mark.parametrize(
@@ -118,15 +328,9 @@ def test_ctms_by_alt_id(sample_contacts, client, alt_id_name, alt_id_value):
         ("mofo_id", "cad092ec-a71a-4df5-aa92-517959caeecb"),
     ],
 )
-def test_ctms_by_alt_id_none_found(sample_contacts, client, alt_id_name, alt_id_value):
+def test_get_ctms_by_alt_id_none_found(client, dbsession, alt_id_name, alt_id_value):
     """An empty list is returned when no contacts have the alternate ID."""
-    maximal_id, contact = sample_contacts["maximal"]
     resp = client.get("/ctms", params={alt_id_name: alt_id_value})
     assert resp.status_code == 200
     data = resp.json()
     assert len(data) == 0
-
-
-def test_empty_db(dbsession):
-    """Database is empty at the start of tests."""
-    assert dbsession.query(Email).count() == 0
