d62766e4bef3dd47ca1b270b166f5494735dbd76
John Whitlock <jwhitlock@mozilla.com>
2021-03-16 11:48:14 -0500

    Switch from .first() to .one_or_none()
    We expect zero or one results for these one-to-one relationships, and
    .one_or_none() will detect issues sooner.

--- ctms/crud.py
+++ ctms/crud.py
@@ -24,15 +24,19 @@ from .schemas import (
 
 
 def get_email_by_email_id(db: Session, email_id: UUID4):
-    return db.query(Email).filter(Email.email_id == email_id).first()
+    return db.query(Email).filter(Email.email_id == email_id).one_or_none()
 
 
 def get_amo_by_email_id(db: Session, email_id: UUID4):
-    return db.query(AmoAccount).filter(AmoAccount.email_id == email_id).first()
+    return db.query(AmoAccount).filter(AmoAccount.email_id == email_id).one_or_none()
 
 
 def get_fxa_by_email_id(db: Session, email_id: UUID4):
-    return db.query(FirefoxAccount).filter(FirefoxAccount.email_id == email_id).first()
+    return (
+        db.query(FirefoxAccount)
+        .filter(FirefoxAccount.email_id == email_id)
+        .one_or_none()
+    )
 
 
 def get_newsletters_by_email_id(db: Session, email_id: UUID4):
@@ -40,7 +44,7 @@ def get_newsletters_by_email_id(db: Session, email_id: UUID4):
 
 
 def get_vpn_by_email_id(db: Session, email_id: UUID4):
-    return db.query(VpnWaitlist).filter(VpnWaitlist.email_id == email_id).first()
+    return db.query(VpnWaitlist).filter(VpnWaitlist.email_id == email_id).one_or_none()
 
 
 def _contact_base_query(db):
