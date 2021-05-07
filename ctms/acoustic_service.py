e5aaf6290b7586f6f67d95ec7d48f6fa80ba3b87
Bryan Sieber <bsieber@mozilla.com>
2021-05-06 21:20:14 -0700

    Updating Acoustic conversion to more rely on the objects, instead of as dictionarys; adding tests

--- ctms/acoustic_service.py
+++ ctms/acoustic_service.py
@@ -1,17 +1,18 @@
+import datetime
 import logging
-import re
+from typing import List
 
 from django.utils.encoding import force_bytes
 from lxml import etree
 from silverpop.api import Silverpop, SilverpopResponseException
 
-from ctms.schemas import ContactSchema
+from ctms.schemas import ContactSchema, NewsletterSchema
 
 logger = logging.getLogger(__name__)
 
 
 class AcousticResources:
-    # TODO: externalize
+    # TODO: externalize, maybe a DB table?
     VALID_ACOUSTIC_MAIN_TABLE_FIELDS = {
         "sfdc_id": 1,
         "email": 1,
@@ -70,6 +71,12 @@ class AcousticResources:
         "test-pilot": "sub_test_pilot",
         "about-mozilla": "sub_about_mozilla",
         "app-dev": "sub_apps_and_hacks",
+        "firefox-welcome": "firefox-welcome",  # TODO: Confirm approach, found newsletters through testing
+        "mozilla-welcome": "mozilla-welcome",  # TODO: Confirm
+        "firefox-os": "firefox-os",  # TODO: Confirm
+        "ambassadors": "ambassadors",  # TODO: Confirm
+        "maker-party": "maker-party",  # TODO: Confirm
+        "mozilla-learning-network": "mozilla-learning-network",  # TODO: Confirm
     }
 
 
@@ -83,14 +90,13 @@ class Acoustic(Silverpop):
 
     @staticmethod
     def _process_response(resp):
-        logger.debug("Response: %s", resp.text)
         response = etree.fromstring(resp.text.encode("utf-8"))
         # success = response.find(".//SUCCESS")
 
         # how main table failures are reported:
         fault = response.find(".//Fault/FaultString")
         if fault is not None:
-            print(fault.text)
+            # print(fault.text) # TODO: Convert to logging
             raise SilverpopResponseException(fault.text)
 
         # how RT failures are reported:    (hooray for consistency.)
@@ -102,7 +108,6 @@ class Acoustic(Silverpop):
         return response
 
     def _call(self, xml):
-        logger.debug("Request: %s", xml)
         # Following basket's suggested pattern,
         # adding force_bytes to deal with some Acoustic rejections
         response = self.session.post(self.api_endpoint, data={"xml": force_bytes(xml)})
@@ -120,7 +125,7 @@ class CTMSToAcousticService:
         server_number=6,
     ):
         """
-        Constuct a CTMSToAcousticService object that can interface between contacts and acoustic forms of data
+        Construct a CTMSToAcousticService object that can interface between contacts and acoustic forms of data
         """
         self.acoustic = Acoustic(
             client_id=client_id,
@@ -128,94 +133,103 @@ class CTMSToAcousticService:
             refresh_token=refresh_token,
             server_number=server_number,
         )
+        if isinstance(acoustic_main_table_id, str):
+            acoustic_main_table_id = int(acoustic_main_table_id)
         self.acoustic_main_table_id = acoustic_main_table_id
-        self.acoustic_newsletter_table_id = acoustic_newsletter_table_id
+        if isinstance(acoustic_newsletter_table_id, str):
+            acoustic_newsletter_table_id = int(acoustic_newsletter_table_id)
+        self.acoustic_newsletter_table_id = int(acoustic_newsletter_table_id)
 
-    def _convert_ctms_to_acoustic(self, ctms_data: dict):
-        # populate with all the sub_flags set to false
-        # they'll get set to true below, as-needed
-        acoustic_main_table = {
-            v: "0" for v in AcousticResources.MAIN_TABLE_SUBSCR_FLAGS.values()
-        }
-
-        # flatten the "sub-dicts" into the main dict to match Acoustic's schema
-        for subdict in ["amo", "fxa", "vpn_waitlist"]:
-            for key, value in ctms_data[subdict].items():
-                acoustic_field_name = subdict + "_" + key
-                if key == "fxa_id":
-                    acoustic_field_name = "fxa_id"
-                if (
-                    acoustic_field_name
-                    in AcousticResources.VALID_ACOUSTIC_MAIN_TABLE_FIELDS
-                ):
-                    acoustic_main_table[
-                        acoustic_field_name
-                    ] = self._transform_field_for_acoustic(value)
-                else:
-                    print(
-                        "Skipping CTMS field ctms_data[%s][%s] because no match in Acoustic"
-                        % (subdict, key)
-                    )
-
-        for key, value in ctms_data["email"].items():
-            acoustic_field_name = key
-            if acoustic_field_name == "primary_email":
-                acoustic_field_name = "email"
-            if (
-                acoustic_field_name
-                in AcousticResources.VALID_ACOUSTIC_MAIN_TABLE_FIELDS
-            ):
-                acoustic_main_table[
-                    acoustic_field_name
-                ] = self._transform_field_for_acoustic(value)
-            else:
-                print(
-                    "Skipping CTMS field ctms_data[email][%s] because no match in Acoustic"
-                    % (key)
-                )
-
-        # create the RT rows for the newsletter table in acoustic
-        newsletter_rows = []
-        for newsletter in ctms_data["newsletters"]:
-            newsletter_rows.append(
-                {
-                    "email_id": ctms_data["email"]["email_id"],
-                    "newsletter_name": newsletter["name"],
-                    "newsletter_format": newsletter["format"],
-                    "newsletter_lang": newsletter["lang"],
-                    "newsletter_source": newsletter["source"],
-                    "newsletter_unsub_reason": newsletter["unsub_reason"],
-                    "create_date": self._transform_field_for_acoustic(
-                        newsletter["create_timestamp"]
-                    ),
-                    "update_date": self._transform_field_for_acoustic(
-                        newsletter["update_timestamp"]
-                    ),
-                }
-            )
-            # and finally flip the main table's sub_<newsletter> flag to true for each subscription
-            if newsletter["subscribed"]:
-                acoustic_main_table[
-                    AcousticResources.MAIN_TABLE_SUBSCR_FLAGS[newsletter["name"]]
-                ] = "1"
-
+    def convert_ctms_to_acoustic(self, contact: ContactSchema):
+        acoustic_main_table = self._main_table_converter(contact)
+        newsletter_rows = self._newsletter_converter(acoustic_main_table, contact)
         return acoustic_main_table, newsletter_rows
 
+    def _main_table_converter(self, contact):
+        acoustic_main_table = {
+            # populate with all the sub_flags set to false
+            # they'll get set to true below, as-needed
+            v: "0"
+            for v in AcousticResources.MAIN_TABLE_SUBSCR_FLAGS.values()
+        }
+        acceptable_subdicts = ["email", "amo", "fxa", "vpn_waitlist"]
+        special_cases = {
+            ("fxa", "fxa_id"): "fxa_id",
+            ("email", "primary_email"): "email",
+        }
+        for contact_attr in contact.dict():
+            subdict_value = getattr(contact, contact_attr)
+            contact_attr_name = str(contact_attr)
+            if contact_attr_name in acceptable_subdicts and subdict_value is not None:
+                for inner_attr in subdict_value.dict():
+                    inner_value = getattr(subdict_value, inner_attr)
+                    inner_attr_name = str(inner_attr)
+                    if contact_attr_name != "email":
+                        acoustic_field_name = contact_attr_name + "_" + inner_attr_name
+                    else:
+                        # Email table records should not be prefixed
+                        acoustic_field_name = inner_attr_name
+
+                    special_key = (contact_attr_name, inner_attr_name)
+                    if special_key in special_cases:
+                        acoustic_field_name = special_cases.get(special_key)
+
+                    if (
+                        acoustic_field_name
+                        in AcousticResources.VALID_ACOUSTIC_MAIN_TABLE_FIELDS
+                    ):
+                        acoustic_main_table[
+                            acoustic_field_name
+                        ] = self.transform_field_for_acoustic(inner_value)
+                    # else:
+                    # print(f"Skipping CTMS field ({contact_attr}, {inner_attr}) because no match "
+                    #       f"in Acoustic") # TODO: Convert to logging
+        return acoustic_main_table
+
+    def _newsletter_converter(self, acoustic_main_table, contact):
+        # create the RT rows for the newsletter table in acoustic
+        newsletter_rows = []
+        contact_newsletters: List[NewsletterSchema] = contact.newsletters
+        for newsletter in contact_newsletters:
+            if newsletter.name in AcousticResources.MAIN_TABLE_SUBSCR_FLAGS.keys():
+                newsletter_dict = newsletter.dict()
+                create_timestamp = datetime.date.today().isoformat()
+                if "create_timestamp" in newsletter_dict:
+                    create_timestamp = newsletter_dict["create_timestamp"]
+                update_timestamp = datetime.date.today().isoformat()
+                if "update_timestamp" in newsletter_dict:
+                    update_timestamp = newsletter_dict["update_timestamp"]
+                newsletter_template = {
+                    "email_id": contact.email.email_id,
+                    "newsletter_name": newsletter.name,
+                    "newsletter_format": newsletter.format,
+                    "newsletter_lang": newsletter.lang,
+                    "newsletter_source": newsletter.source,
+                    "newsletter_unsub_reason": newsletter.unsub_reason,
+                    "create_date": create_timestamp,
+                    "update_date": update_timestamp,
+                }
+                newsletter_rows.append(newsletter_template)
+                # and finally flip the main table's sub_<newsletter> flag to true for each subscription
+                if newsletter.subscribed:
+                    acoustic_main_table[
+                        AcousticResources.MAIN_TABLE_SUBSCR_FLAGS[newsletter.name]
+                    ] = "1"
+            # else:
+            # print(f"Skipping Newsletter {newsletter.name} because no match in Acoustic") # TODO: Convert to logging
+        return newsletter_rows
+
     @staticmethod
-    def _transform_field_for_acoustic(data):
+    def transform_field_for_acoustic(data):
         if isinstance(data, bool):
             if data:
                 return "1"
             return "0"
-        # Acoustic doesn't have timestamps, so make timestamps into dates.
-        # Is this appropriate for all TS fields?
-        # Is the below RE appropriate for what we'll actually be getting from Postgres?
-        try:
-            matching = re.search(r"^(\d{4}-\d{2}-\d{2})T\d", data)
-            if matching:
-                return matching.groups()[0]
-        except TypeError:
-            pass
+        if isinstance(data, datetime.datetime):
+            # Acoustic doesn't have timestamps, so make timestamps into dates.
+            return data.date().isoformat()
+        if isinstance(data, datetime.date):
+            return data.isoformat()
         return data
 
     def _add_contact(self, list_id=None, sync_fields=None, columns=None):
@@ -247,7 +261,7 @@ class CTMSToAcousticService:
         :return: Boolean indicating True (success) or False (failure)
         """
         try:
-            main_table_data, nl_data = self._convert_ctms_to_acoustic(contact.dict())
+            main_table_data, nl_data = self.convert_ctms_to_acoustic(contact)
 
             main_table_id = self.acoustic_main_table_id
             self._add_contact(
