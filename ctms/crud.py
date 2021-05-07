f09e9508a518814a686c3cce9d1ee47889901dc2
Bryan Sieber <bsieber@mozilla.com>
2021-05-07 12:54:56 -0700

    Additional Tests. Seperating background running from sync class. Might need to ask for naming-assistance.

--- ctms/crud.py
+++ ctms/crud.py
@@ -307,14 +307,16 @@ def schedule_acoustic_record(
 
 
 def retry_acoustic_record(db: Session, pending_record: PendingAcousticRecord) -> None:
+    if pending_record.retry is None:
+        pending_record.retry = 0
     pending_record.retry += 1
     pending_record.update_timestamp = datetime.now(timezone.utc)
-    # db.commit() # TODO: will this be done by the background job?
+    # db.commit() # TODO: this will be done by the background job
 
 
 def delete_acoustic_record(db: Session, pending_record: PendingAcousticRecord) -> None:
     db.delete(pending_record)
-    # db.commit() # TODO: will this be done by the background job?
+    # db.commit() # TODO: this will be done by the background job
 
 
 def create_amo(
---
+++ tests/unit/test_acoustic_sync.py
@@ -0,0 +1,128 @@
+import datetime
+from unittest import mock
+from unittest.mock import MagicMock
+
+import pytest
+
+from ctms import acoustic_service
+
+CTMS_ACOUSTIC_CLIENT_ID = "CLIENT"
+CTMS_ACOUSTIC_CLIENT_SECRET = "SECRET"
+CTMS_ACOUSTIC_REFRESH_TOKEN = "REFRESH"
+CTMS_ACOUSTIC_MAIN_TABLE_ID = "1"
+CTMS_ACOUSTIC_NEWSLETTER_TABLE_ID = "9"
+
+
+@pytest.fixture()
+def no_acoustic():
+    patcher = mock.patch("ctms.acoustic_service.Acoustic")
+    patcher.start()
+    yield patcher
+    patcher.stop()
+
+
+@pytest.fixture
+def base_ctms_acoustic_service(no_acoustic):
+    return acoustic_service.CTMSToAcousticService(
+        client_id=CTMS_ACOUSTIC_CLIENT_ID,
+        client_secret=CTMS_ACOUSTIC_CLIENT_SECRET,
+        refresh_token=CTMS_ACOUSTIC_REFRESH_TOKEN,
+        acoustic_main_table_id=CTMS_ACOUSTIC_MAIN_TABLE_ID,
+        acoustic_newsletter_table_id=CTMS_ACOUSTIC_NEWSLETTER_TABLE_ID,
+        server_number=6,
+    )
+
+
+def test_base_service_creation(base_ctms_acoustic_service):
+    assert base_ctms_acoustic_service is not None
+
+
+def test_ctms_to_acoustic(
+    base_ctms_acoustic_service, example_contact, maximal_contact, minimal_contact
+):
+    contact_list = [example_contact, maximal_contact, minimal_contact]
+    example_contact_expected = [54, len(example_contact.newsletters)]
+    maximal_contact_expected = [54, len(maximal_contact.newsletters)]
+    minimal_contact_expected = [36, len(minimal_contact.newsletters)]
+    expected_results = {
+        example_contact.email.email_id: example_contact_expected,
+        maximal_contact.email.email_id: maximal_contact_expected,
+        minimal_contact.email.email_id: minimal_contact_expected,
+    }
+
+    for contact in contact_list:
+        expected = expected_results.get(contact.email.email_id)
+        _main, _newsletter = base_ctms_acoustic_service.convert_ctms_to_acoustic(
+            contact
+        )
+        assert _main is not None
+        assert _newsletter is not None
+        assert (
+            len(_main) == expected[0]
+        ), f"Expected {expected[0]} with id {contact.email.email_id}"
+        assert (
+            len(_newsletter) == expected[1]
+        ), f"Expected {expected[1]} with id {contact.email.email_id}"
+        assert _main["email"] == contact.email.primary_email
+        if contact.fxa is not None:
+            assert _main["fxa_id"] == contact.fxa.fxa_id
+        for row in _newsletter:
+            assert (
+                row["email_id"] == contact.email.email_id
+            )  # TODO: Should this be a str?
+            assert row["newsletter_name"] is not None
+
+
+def test_ctms_to_acoustic_mocked(
+    no_acoustic,
+    base_ctms_acoustic_service,
+    maximal_contact,
+):
+    acoustic_mock: MagicMock = MagicMock()
+    base_ctms_acoustic_service.acoustic = acoustic_mock
+    _main, _newsletter = base_ctms_acoustic_service.convert_ctms_to_acoustic(
+        maximal_contact
+    )  # To be used as in testing, for expected inputs to downstream methods
+    assert _main is not None
+    assert _newsletter is not None
+
+    results = base_ctms_acoustic_service.attempt_to_upload_ctms_contact(maximal_contact)
+    assert results  # success
+    acoustic_mock.add_recipient.assert_called()
+    acoustic_mock.insert_update_relational_table.assert_called()
+
+    acoustic_mock.add_recipient.assert_called_with(
+        list_id=int(CTMS_ACOUSTIC_MAIN_TABLE_ID),
+        created_from=3,
+        update_if_found="TRUE",
+        allow_html=False,
+        sync_fields={"email_id": _main["email_id"]},
+        columns=_main,
+    )
+
+    acoustic_mock.insert_update_relational_table.assert_called_with(
+        table_id=int(CTMS_ACOUSTIC_NEWSLETTER_TABLE_ID), rows=_newsletter
+    )
+
+
+def test_transform_field(base_ctms_acoustic_service):
+    is_true = base_ctms_acoustic_service.transform_field_for_acoustic(True)
+    assert is_true == "1"
+    is_false = base_ctms_acoustic_service.transform_field_for_acoustic(False)
+    assert is_false == "0"
+    is_datetime = base_ctms_acoustic_service.transform_field_for_acoustic(
+        datetime.datetime.now()
+    )
+    assert is_datetime is not None
+    is_date = base_ctms_acoustic_service.transform_field_for_acoustic(
+        datetime.date.today()
+    )
+    assert is_date is not None
+
+    try:
+        is_datetime = datetime.date.fromisoformat(is_datetime)
+        assert isinstance(is_datetime, datetime.date)
+        is_date = datetime.date.fromisoformat(is_date)
+        assert isinstance(is_date, datetime.date)
+    except:  # pylint: disable=W0702
+        assert False, "Failure with timestamp validation"
