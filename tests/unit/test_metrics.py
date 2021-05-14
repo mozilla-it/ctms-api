dadbd23dafa3d5c773ce62d3bfef8480d7077e6d
John Whitlock <jwhitlock@mozilla.com>
2021-05-14 11:53:06 -0500

    Setup MP metrics registry in /metrics endpoint
    For each process, use a separate CollectorRegistry initialized at
    startup.
    
    For the /metrics view, use this collecter in single-process mode, but
    use a fresh CollectorRegistry with MultiProcessCollector in
    multiprocess under gunicorn. This should avoid duplicate metric exports
    (one for multiprocess, one for the process serving the request).

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -2,14 +2,18 @@
 from unittest.mock import Mock, patch
 
 import pytest
-from prometheus_client import REGISTRY, CollectorRegistry, generate_latest
+from prometheus_client import CollectorRegistry, generate_latest
 from prometheus_client.multiprocess import MultiProcessCollector
 from prometheus_client.parser import text_string_to_metric_families
 from pydantic import ValidationError
 from structlog.testing import capture_logs
 
 from ctms.app import app
-from ctms.metrics import init_metrics, init_metrics_labels, init_metrics_registry
+from ctms.metrics import (
+    get_metrics_reporting_registry,
+    init_metrics,
+    init_metrics_labels,
+)
 
 # Metric cardinatility numbers
 # These numbers change as routes are added or changed
@@ -56,33 +60,35 @@ def metrics(setup_metrics):
     return test_metrics
 
 
-def test_init_metrics_registry_standard():
-    """init_metrics_registry() returns a registry."""
+def test_get_metrics_reporting_registry_standard():
+    """get_metrics_reporting_registry() returns the passed registry."""
+    passed_registry = CollectorRegistry()
     with patch("ctms.metrics.config.Settings") as settings:
         settings.return_value.prometheus_multiproc_dir = None
-        the_registry = init_metrics_registry()
-    assert the_registry != REGISTRY
-    assert the_registry._collector_to_names == {}  # pylint: disable=protected-access
+        the_registry = get_metrics_reporting_registry(passed_registry)
+    assert the_registry is passed_registry
 
 
-def test_init_metrics_registry_multiprocess(tmp_path):
-    """init_metrics_registry() can register a multiprocessing collector."""
+def test_get_metrics_reporting_registry_multiprocess(tmp_path):
+    """get_metrics_reporting_registry() can register a multiprocessing collector."""
+    passed_registry = CollectorRegistry()
     with patch("ctms.metrics.config.Settings") as settings:
         settings.return_value.prometheus_multiproc_dir = tmp_path
-        the_registry = init_metrics_registry()
-    assert the_registry != REGISTRY
+        the_registry = get_metrics_reporting_registry(passed_registry)
+    assert the_registry is not passed_registry
     # pylint: disable=protected-access
     collectors = list(the_registry._collector_to_names.keys())
     assert len(collectors) == 1
     assert isinstance(collectors[0], MultiProcessCollector)
 
 
-def test_init_metrics_registry_settings_error():
-    """init_metrics_registry() handles invalid settings."""
+def test_get_metrics_reporting_registry_settings_error():
+    """get_metrics_reporting_registry() handles invalid settings."""
+    passed_registry = CollectorRegistry()
     with patch("ctms.metrics.config.Settings") as settings:
         settings.side_effect = ValidationError(errors=[], model=Mock)
-        the_registry = init_metrics_registry()
-    assert the_registry != REGISTRY
+        the_registry = get_metrics_reporting_registry(passed_registry)
+    assert the_registry is passed_registry
     assert the_registry._collector_to_names == {}  # pylint: disable=protected-access
 
 
