88fdb711fb7190c33ff0aa89a68a6473105b9701
Bryan Sieber <bsieber@mozilla.com>
2021-05-25 13:40:07 -0700

    Updating labels to forcibly add k8s, per slack msg: https://mozilla.slack.com/archives/G01M9NUSUG1/p1620932758230200?thread_ts=1620928446.225800&cid=G01M9NUSUG1

--- ctms/background_metrics.py
+++ ctms/background_metrics.py
@@ -12,46 +12,89 @@ class BackgroundMetricService:  # pylint: disable=too-many-instance-attributes
         self.requests = Counter(
             registry=registry,
             name="acoustic_request_total",
-            documentation="Total count of requests by method, path, status code, and status code family.",
+            documentation="Total count of acoustic requests by method, and status.",
             labelnames=[
                 "method",
                 "status",
+                "app_kubernetes_io_component",
+                "app_kubernetes_io_instance",
+                "app_kubernetes_io_name",
             ],
         )
+        self.app_kubernetes_io_component = "web"
+        self.app_kubernetes_io_instance = "ctms"
+        self.app_kubernetes_io_name = "ctms"
+
         self.requests_duration = Histogram(
             registry=registry,
             name="acoustic_requests_duration",
-            documentation="Histogram of requests processing time by path (in seconds)",
-            labelnames=["method", "status"],
+            documentation="Histogram of requests processing time by method (in seconds)",
+            labelnames=[
+                "method",
+                "status",
+                "app_kubernetes_io_component",
+                "app_kubernetes_io_instance",
+                "app_kubernetes_io_name",
+            ],
             buckets=(0.01, 0.05, 0.1, 0.5, 1, 5, 10, INF),
         )
         self.sync_requests = Counter(
             registry=registry,
             name="acoustic_sync_total",
+            labelnames=[
+                "app_kubernetes_io_component",
+                "app_kubernetes_io_instance",
+                "app_kubernetes_io_name",
+            ],
             documentation="Total count of contacts synced to acoustic.",
         )
         self.backlog_gauge = Gauge(
             registry=registry,
             name="acoustic_sync_backlog",
-            documentation="Gauge of the number of contacts in the sync backlog.",
+            labelnames=[
+                "app_kubernetes_io_component",
+                "app_kubernetes_io_instance",
+                "app_kubernetes_io_name",
+            ],
+            documentation="Gauge of the number of contacts in the sync backlog. Not counting over-retried records.",
         )
         self.pushgateway_url = pushgateway_url
         self.job_name = "prometheus-pushgateway"
 
     def inc_acoustic_request_total(self, method, status):
-        self.requests.labels(method=method, status=status).inc()
+        self.requests.labels(
+            method=method,
+            status=status,
+            app_kubernetes_io_component=self.app_kubernetes_io_component,
+            app_kubernetes_io_instance=self.app_kubernetes_io_instance,
+            app_kubernetes_io_name=self.app_kubernetes_io_name,
+        ).inc()
         self.logger.debug("Incrementing acoustic request")
 
     def observe_acoustic_request_duration(self, method, status, duration_s):
-        self.requests_duration.labels(method=method, status=status).observe(duration_s)
+        self.requests_duration.labels(
+            method=method,
+            status=status,
+            app_kubernetes_io_component=self.app_kubernetes_io_component,
+            app_kubernetes_io_instance=self.app_kubernetes_io_instance,
+            app_kubernetes_io_name=self.app_kubernetes_io_name,
+        ).observe(duration_s)
         self.logger.debug("Observing acoustic request duration")
 
     def inc_acoustic_sync_total(self):
-        self.sync_requests.inc()
+        self.sync_requests.labels(
+            app_kubernetes_io_component=self.app_kubernetes_io_component,
+            app_kubernetes_io_instance=self.app_kubernetes_io_instance,
+            app_kubernetes_io_name=self.app_kubernetes_io_name,
+        ).inc()
         self.logger.debug("Incrementing sync'd records")
 
     def gauge_acoustic_sync_backlog(self, value):
-        self.backlog_gauge.set(value)
+        self.backlog_gauge.labels(
+            app_kubernetes_io_component=self.app_kubernetes_io_component,
+            app_kubernetes_io_instance=self.app_kubernetes_io_instance,
+            app_kubernetes_io_name=self.app_kubernetes_io_name,
+        ).set(value)
         self.logger.debug("Setting backlog gauge")
 
     def push_to_gateway(self):
