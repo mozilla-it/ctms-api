2ca579f08abc9dfac7efec4512362f7fa173ed18
John Whitlock <jwhitlock@mozilla.com>
2021-10-20 15:30:53 -0500

    Refactor tests for fixtures

--- tests/unit/test_crud_stripe.py
+++ tests/unit/test_crud_stripe.py
@@ -18,6 +18,20 @@ from ctms.schemas import (
 
 
 @pytest.fixture
+def stripe_customer(dbsession, example_contact):
+    customer = StripeCustomerCreateSchema(
+        stripe_id="cus_8epDebVEl8Bs2V",
+        stripe_created=datetime.now(tz=timezone.utc),
+    )
+    db_customer = create_stripe_customer(
+        dbsession, example_contact.email.email_id, customer
+    )
+    dbsession.commit()
+    dbsession.refresh(db_customer)
+    return db_customer
+
+
+@pytest.fixture
 def stripe_product(dbsession):
     product = StripeProductCreateSchema(
         stripe_id="prod_KPReWHqwGqZBzc",
@@ -31,37 +45,8 @@ def stripe_product(dbsession):
     return db_product
 
 
-def test_create_stripe_customer(dbsession, example_contact):
-    customer = StripeCustomerCreateSchema(
-        stripe_id="cus_8epDebVEl8Bs2V",
-        stripe_created=datetime.now(tz=timezone.utc),
-    )
-    db_customer = create_stripe_customer(
-        dbsession, example_contact.email.email_id, customer
-    )
-    assert db_customer
-    dbsession.commit()  # Does not throw
-    dbsession.refresh(db_customer)
-    out_customer = StripeCustomerOutputSchema.from_orm(db_customer)
-    assert out_customer.stripe_id == "cus_8epDebVEl8Bs2V"
-
-
-def test_create_stripe_product(dbsession):
-    product = StripeProductCreateSchema(
-        stripe_id="prod_KPReWHqwGqZBzc",
-        stripe_created=datetime.now(tz=timezone.utc),
-        stripe_updated=datetime.now(tz=timezone.utc),
-        name="Mozilla ISP",
-    )
-    db_product = create_stripe_product(dbsession, product)
-    assert db_product
-    dbsession.commit()  # Does not throw
-    dbsession.refresh(db_product)
-    out_product = StripeProductOutputSchema.from_orm(db_product)
-    assert out_product.stripe_id == "prod_KPReWHqwGqZBzc"
-
-
-def test_create_stripe_price_recurring(dbsession, stripe_product):
+@pytest.fixture
+def stripe_price(dbsession, stripe_product):
     price = StripePriceCreateSchema(
         stripe_id="price_1Jkcl3Kb9q6OnNsLFbECmMtd",
         stripe_created=datetime.now(tz=timezone.utc),
@@ -74,7 +59,21 @@ def test_create_stripe_price_recurring(dbsession, stripe_product):
     assert db_price
     dbsession.commit()  # Does not throw
     dbsession.refresh(db_price)
-    out_price = StripePriceOutputSchema.from_orm(db_price)
+    return db_price
+
+
+def test_create_stripe_customer_by_fixture(stripe_customer):
+    out_customer = StripeCustomerOutputSchema.from_orm(stripe_customer)
+    assert out_customer.stripe_id == "cus_8epDebVEl8Bs2V"
+
+
+def test_create_stripe_product_by_fixture(stripe_product):
+    out_product = StripeProductOutputSchema.from_orm(stripe_product)
+    assert out_product.stripe_id == "prod_KPReWHqwGqZBzc"
+
+
+def test_create_stripe_price_from_fixture(dbsession, stripe_price):
+    out_price = StripePriceOutputSchema.from_orm(stripe_price)
     assert out_price.stripe_id == "price_1Jkcl3Kb9q6OnNsLFbECmMtd"
 
 
