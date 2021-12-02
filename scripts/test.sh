afd0427a34667cfe47d8c45debbf73ae7a7aaa8b
Bryan Sieber <bsieber@mozilla.com>
2021-12-01 16:17:29 -0800

    Comment removal in  test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,4 +8,3 @@ BASE_DIR="$(dirname "$CURRENT_DIR")"
 coverage run --rcfile "${BASE_DIR}/pyproject.toml" -m pytest
 coverage report --rcfile "${BASE_DIR}/pyproject.toml" -m --fail-under 80
 coverage html --rcfile "${BASE_DIR}/pyproject.toml"
-# Used in GH actions for testing
