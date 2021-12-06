bc120bd3f1d0de49f304f74a7ea2e2a338f3603d
John Whitlock <jwhitlock@mozilla.com>
2021-12-06 14:59:45 -0600

    Prepare for v1.1.1 release

--- ctms/app.py
+++ ctms/app.py
@@ -81,7 +81,7 @@ from .schemas import (
 app = FastAPI(
     title="ConTact Management System (CTMS)",
     description="CTMS API (work in progress)",
-    version="1.1.0",
+    version="1.1.1",
 )
 SessionLocal = None
 METRICS_REGISTRY = CollectorRegistry()
