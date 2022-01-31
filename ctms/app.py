568235c7dc9373ed4ad1e6c4121e47a2669e6845
Bryan Sieber <bsieber@mozilla.com>
2022-01-31 11:48:40 -0800

    Version Bump

--- ctms/app.py
+++ ctms/app.py
@@ -88,7 +88,7 @@ from .schemas import (
 app = FastAPI(
     title="ConTact Management System (CTMS)",
     description="CTMS API (work in progress)",
-    version="1.1.4",
+    version="1.1.5",
 )
 SessionLocal = None
 METRICS_REGISTRY = CollectorRegistry()
