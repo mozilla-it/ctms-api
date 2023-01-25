84cb066d87e0c55a6dae201f0ac1b5a96c9f0d0f
Graham Beckley <gbeckley@mozilla.com>
2023-01-25 17:41:11 -0500

    Move database access to globals
    Since create_engine and sessionmaker return factories, there's no need
    to initialize them on app startup -- we can just store them in the global
    scope of the database module

--- ctms/database.py
+++ ctms/database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from . import config
 
 
-def get_db_engine(settings: config.Settings):
-    engine = create_engine(  # https://docs.sqlalchemy.org/en/14/core/engines.html#engine-creation-api
+def engine_factory(settings):
+    return create_engine(
         settings.db_url,
         pool_size=settings.db_pool_size,
         max_overflow=settings.db_max_overflow,
@@ -13,8 +13,9 @@ def get_db_engine(settings: config.Settings):
         pool_recycle=settings.db_pool_recycle_in_seconds,
         echo=settings.log_sqlalchemy,
     )
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    return engine, SessionLocal
 
+
+engine = engine_factory(config.Settings())
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
