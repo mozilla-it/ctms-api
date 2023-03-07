4d7db9664ae36f28dad30f140f601bd4e4f09021
Mathieu Leplatre <mathieu@mozilla.com>
2023-03-07 13:43:16 +0100

    Add unit tests for cascade in DB and ORM

--- tests/unit/test_crud.py
+++ tests/unit/test_crud.py
@@ -5,6 +5,7 @@ from typing import List
 from uuid import uuid4
 
 import pytest
+import sqlalchemy
 
 from ctms.crud import (
     create_amo,
@@ -31,7 +32,15 @@ from ctms.crud import (
     retry_acoustic_record,
     schedule_acoustic_record,
 )
-from ctms.models import Email, PendingAcousticRecord
+from ctms.models import (
+    AmoAccount,
+    Email,
+    FirefoxAccount,
+    MozillaFoundationContact,
+    Newsletter,
+    PendingAcousticRecord,
+    Waitlist,
+)
 from ctms.schemas import (
     AddOnsInSchema,
     EmailInSchema,
@@ -989,3 +998,51 @@ def test_relations_on_stripe_subscription_items(
     assert subscription_item.subscription == subscription
     assert subscription_item.price == price
     assert subscription_item.get_email_id() == email_id
+
+
+@pytest.mark.parametrize(
+    "model",
+    (
+        PendingAcousticRecord,
+        AmoAccount,
+        MozillaFoundationContact,
+        Newsletter,
+        Waitlist,
+        FirefoxAccount,
+    ),
+)
+def test_delete_happens_in_cascade_in_db(dbsession, maximal_contact, model):
+    """Verifies that delete happens in cascade"""
+    email_id = maximal_contact.email.email_id
+    schedule_acoustic_record(dbsession, email_id)
+    dbsession.flush()
+
+    dbsession.query(Email).filter(Email.email_id == email_id).delete()
+
+    assert not (dbsession.query(model).filter(model.email_id == email_id).all())
+
+
+@pytest.mark.parametrize(
+    "model",
+    (
+        PendingAcousticRecord,
+        AmoAccount,
+        MozillaFoundationContact,
+        Newsletter,
+        Waitlist,
+        FirefoxAccount,
+    ),
+)
+def test_cascade_is_reflected_in_orm(dbsession, maximal_contact, model):
+    email_id = maximal_contact.email.email_id
+    schedule_acoustic_record(dbsession, email_id)
+    dbsession.flush()
+
+    instance = dbsession.query(model).filter(model.email_id == email_id).first()
+    assert instance.email
+
+    dbsession.query(Email).filter(Email.email_id == email_id).delete()
+    dbsession.commit()
+
+    with pytest.raises(sqlalchemy.orm.exc.ObjectDeletedError):
+        instance.email
