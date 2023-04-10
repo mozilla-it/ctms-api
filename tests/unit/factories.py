5730931849d45f08e43a857fd7cea14ecb1577ac
Graham Beckley <gbeckley@mozilla.com>
2023-04-10 09:57:48 -0400

    Add fxa, amo, and mofo factories

--- tests/unit/factories.py
+++ tests/unit/factories.py
@@ -1,5 +1,6 @@
 import factory
 from factory.alchemy import SQLAlchemyModelFactory
+from faker import Faker
 
 from ctms import models
 from ctms.database import ScopedSessionLocal
@@ -35,6 +36,51 @@ class WaitlistFactory(BaseSQLAlchemyModelFactory):
     email = factory.SubFactory(factory="tests.unit.factories.EmailFactory")
 
 
+class FirefoxAccountFactory(BaseSQLAlchemyModelFactory):
+    class Meta:
+        model = models.FirefoxAccount
+
+    fxa_id = factory.Faker("uuid4")
+    primary_email = factory.Faker("email")
+    created_date = factory.Faker("date")
+    lang = factory.Faker("language_code")
+    first_service = factory.Faker("word")
+    account_deleted = False
+
+    email = factory.SubFactory(factory="tests.unit.factories.EmailFactory")
+
+
+class AmoAccountFactory(BaseSQLAlchemyModelFactory):
+    class Meta:
+        model = models.AmoAccount
+
+    add_on_ids = factory.LazyAttribute(
+        lambda _: ",".join([Faker().word() for _ in range(2)])
+    )
+    display_name = factory.Faker("name")
+    email_opt_in = True
+    language = factory.Faker("language_code")
+    last_login = factory.Faker("date")
+    location = factory.Faker("city")
+    profile_url = factory.Faker("url")
+    user = True
+    user_id = factory.Faker("uuid4")
+    username = factory.Faker("user_name")
+
+    email = factory.SubFactory(factory="tests.unit.factories.EmailFactory")
+
+
+class MozillaFoundationContactFactory(BaseSQLAlchemyModelFactory):
+    class Meta:
+        model = models.MozillaFoundationContact
+
+    mofo_email_id = factory.Faker("email")
+    mofo_contact_id = factory.Faker("uuid4")
+    mofo_relevant = True
+
+    email = factory.SubFactory(factory="tests.unit.factories.EmailFactory")
+
+
 class EmailFactory(BaseSQLAlchemyModelFactory):
     class Meta:
         model = models.Email
@@ -49,6 +95,27 @@ class EmailFactory(BaseSQLAlchemyModelFactory):
     email_lang = factory.Faker("language_code")
     double_opt_in = False
     has_opted_out_of_email = False
+
+    @factory.post_generation
+    def fxa(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            FirefoxAccountFactory(email=self, **kwargs)
+
+    @factory.post_generation
+    def amo(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            AmoAccountFactory(email=self, **kwargs)
+
+    @factory.post_generation
+    def mofo(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            MozillaFoundationContactFactory(email=self, **kwargs)
 
     @factory.post_generation
     def newsletters(self, create, extracted, **kwargs):
@@ -84,5 +151,8 @@ class EmailFactory(BaseSQLAlchemyModelFactory):
 __all__ = (
     "NewsletterFactory",
     "WaitlistFactory",
+    "AmoAccountFactory",
+    "FirefoxAccountFactory",
+    "MozillaFoundationContactFactory",
     "EmailFactory",
 )
