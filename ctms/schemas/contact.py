9852f37d3bca167ba3dc5a12a1b825ee4e311ba8
Graham Beckley <gbeckley@mozilla.com>
2023-04-27 11:23:11 -0400

    Add comment about `relay_waitlist` property of contact schema

--- ctms/schemas/contact.py
+++ ctms/schemas/contact.py
@@ -244,6 +244,10 @@ class CTMSResponse(BaseModel):
                     geo=waitlist.fields.get("geo"),
                     platform=waitlist.fields.get("platform"),
                 )
+            # If multiple `relay-` waitlists are present, the `geo` field of the
+            # first waitlist is set as the value of `relay_waitlist["geo"]`. This
+            # property is intended for legacy consumers. New consumers should prefer the
+            # `waitlists` property of the contact schema
             if (
                 waitlist.name.startswith("relay")
                 and kwargs["relay_waitlist"].geo is None
