c6e3c42e37f305bb2a4a301a1ccc6c726fb684b6
Graham Beckley <gbeckley@mozilla.com>
2024-07-22 20:30:31 -0400

    Remove usage of parse_obj

--- tests/unit/routers/contacts/test_bulk.py
+++ tests/unit/routers/contacts/test_bulk.py
@@ -110,7 +110,7 @@ def test_get_ctms_bulk_by_timerange(
     assert "items" in results
     assert len(results["items"]) > 0
     dict_contact_expected = sorted_list[1].model_dump()
-    dict_contact_actual = CTMSResponse.parse_obj(results["items"][0]).model_dump()
+    dict_contact_actual = CTMSResponse(**results["items"][0]).model_dump()
     # The response shows computed fields for retro-compat. Contact schema
     # does not have them.
     del dict_contact_actual["vpn_waitlist"]
