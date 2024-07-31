e9d794ff112af8d478f3399535ce61d7bfcc8c91
Graham Beckley <gbeckley@mozilla.com>
2024-07-31 14:12:17 -0400

    Build newsletters in schema tests, instead of create

--- tests/unit/schemas/test_newsletter.py
+++ tests/unit/schemas/test_newsletter.py
@@ -5,11 +5,11 @@ from ctms.schemas import NewsletterSchema
 
 
 def test_source_url_supports_localhost(newsletter_factory):
-    newsletter = newsletter_factory(source="http://localhost:8888/v1")
+    newsletter = newsletter_factory.build(source="http://localhost:8888/v1")
     NewsletterSchema.model_validate(newsletter)
 
 
 def test_source_url_does_not_support_arbitrary_string(newsletter_factory):
-    newsletter = newsletter_factory(source="foobar")
+    newsletter = newsletter_factory.build(source="foobar")
     with pytest.raises(ValidationError):
         NewsletterSchema.model_validate(newsletter)
