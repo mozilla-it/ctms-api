32a85824e0650199767af3c2aeb7c903ba5c9463
Mathieu Leplatre <mathieu@mozilla.com>
2024-08-06 17:01:20 +0200

    Do not reconfigure logs using Alembic config

--- migrations/env.py
+++ migrations/env.py
@@ -14,7 +14,8 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+if not context.config.attributes.get("unit-tests", False):
+    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
